ec956af0b5bc363464022d2db6b633b9c96d8deb
Inna Boiko <146464243+InnaIvBoiko@users.noreply.github.com>
2026-06-05 09:47:51 +0200

    fix: handle errors in GET users API endpoint

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -2,9 +2,21 @@ import { prisma } from '@/prisma/prisma-client';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-    const users = await prisma.user.findMany();
-
-    return NextResponse.json(users);
+    try {
+        const users = await prisma.user.findMany();
+        return NextResponse.json(users);
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            return NextResponse.json(
+                { error: 'Could not retrieve users', details: error.message },
+                { status: 500 }
+            );
+        }
+        return NextResponse.json(
+            { error: 'Could not retrieve users', details: 'Unknown error' },
+            { status: 500 }
+        );
+    }
 }
 
 export async function POST(request: Request) {
